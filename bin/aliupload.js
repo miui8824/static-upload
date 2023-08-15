88c9ede68fba484e5f7b802a0dccbdaee03e5e29
forget <fhs8825@gmail.com>
2023-08-15 17:34:30 +0800

    feat: 阿里云上传添加过期时间控制

--- bin/aliupload.js
+++ bin/aliupload.js
@@ -23,7 +23,7 @@ const ossUpload = async (opts = {}) => {
 
 	let uploadFiles;
 
-	function bouncer(arr) {
+	function bouncer (arr) {
 		// 请把你的代码写在这里
 		return arr.filter((item) => !!item);
 	}
@@ -46,8 +46,20 @@ const ossUpload = async (opts = {}) => {
 		uploadFiles = filearr;
 	}
 	debug('uploadFiles => %j', uploadFiles);
+	// 刷新缓存
+	async function refreshCache (fileUrl) {
+		try {
+			// 修改文件的元信息
+			await client.putMeta(fileUrl, {
+				'refresh': 'true'
+			});
+			console.log('刷新缓存成功');
+		} catch (error) {
+			console.error('刷新缓存失败', error);
+		}
+	}
 
-	let result = await co(function*() {
+	let result = await co(function* () {
 		const successFiles = [];
 		for (const filename of uploadFiles) {
 			const start = Date.now();
@@ -58,12 +70,20 @@ const ossUpload = async (opts = {}) => {
 			try {
 				const result = yield client.putStream(bucketPath, stream, {
 					contentLength: size,
+					headers: {
+						'Cache-Control': `max-age=${opts.maxage || 3600}`,
+						Expires: opts.maxage || 3600,
+					}
 				});
 				successFiles.push({
 					name: result.name,
 					url: result.url,
 				});
 				console.log(`${filename} was uploaded successfully, take ${Date.now() - start}ms`);
+				if (opts.isrefreshCache) {
+					// console.log(result,83)
+					refreshCache(result.name)
+				}
 			} catch (e) {
 				console.log(clc.red(`${filename} upload fail, message: ${e.message}`));
 			}
